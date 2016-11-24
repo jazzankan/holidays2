1bf46eabb898116a47abac03dca4a12d7693e76c
Anders <shasfn@gmail.com>
2016-11-24 21:16:03 +0100

    Första commit, men redan ändrad css

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anders
+ * Date: 2016-11-21
+ * Time: 22:06
+ */
+require_once("Calendar.php");
+$calendar = new Calendar();
+$calendar = $calendar->show();
+$html = <<<EOD
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/2style.css">
+    <title>Semesterlistan</title>
+</head>
+<body>
+$calendar
+</body>
+</html>
+EOD;
+echo $html;
